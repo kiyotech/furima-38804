01a594dddf78c684fa4ab0b25280f6db2de404a7
kiyotech <kiyosawatech@gmail.com>
2023-01-11 00:02:47 +0900

    商品情報編集機能作成

--- app/controllers/items_controller.rb
+++ app/controllers/items_controller.rb
@@ -22,6 +22,22 @@ class ItemsController < ApplicationController
     @item = Item.find(params[:id])
   end
 
+  def edit
+    @item = Item.find(params[:id])
+    return if @item.user == current_user
+
+    redirect_to root_path
+  end
+
+  def update
+    @item = Item.find(params[:id])
+    if @item.update(item_params)
+      redirect_to item_path(@item.id), method: :get
+    else
+      render :edit
+    end
+  end
+
   private
 
   def item_params
